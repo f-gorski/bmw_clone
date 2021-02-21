c9fc6182801e359cf9722273739f7bbaa8563936
Fryderyk Górski <fryderykgorski@gmail.com>
2021-02-21 15:45:05 +0100

    feature: hero section

---
+++ src/Layout/Home/Hero.js
@@ -0,0 +1,45 @@
+import { useState } from 'react';
+import { Link as LinkScroll } from 'react-scroll';
+
+const Hero = () => {
+
+    const [slide, setSlide] = useState("slide1");
+
+    const handleClick = (e) => {
+        const id = e.target.id;
+        setSlide(id);
+    }
+
+    return (
+        <section id="hero" className={"hero " + slide}>
+            <div className="container">
+                <div className="nav-desktop">
+                    <ul className="nav__items">
+                        <li className="nav__item">Nowe BMW iX</li>
+                        <li className="nav__item">Umów jazdę próbną</li>
+                    </ul>
+                    <hr className="nav__line" />
+                    <div className="slogan"><span className="bold">Radość</span> z jazdy</div>
+                </div>
+                <img src={require('../../assets/bmw_logo.svg').default} alt="logo BMW" className="bmw-logo" />
+
+                <div className="hero__content">
+                    <img src={require('../../assets/bmw_i_logo.svg').default} alt="logo BMW" className="hero__logo" />
+                    <h1 className="hero__header">Nowe BMW iX</h1>
+                    <h2 className="hero__subheader">PIONIER NOWEJ ERY.</h2>
+                </div>
+                <div className="cta">
+                    <LinkScroll className="cta-button" smooth={true} to="contact"><button className="cta-button">UMÓW JAZDĘ PRÓBNĄ</button></LinkScroll>
+                    <div className="slider-bars">
+                        <div className="slider-bar" id="slide1" onClick={handleClick}></div>
+                        <div className="slider-bar" id="slide2" onClick={handleClick}></div>
+                        <div className="slider-bar" id="slide3" onClick={handleClick}></div>
+                    </div>
+                </div>
+
+            </div>
+        </section>
+    )
+}
+
+export default Hero;
