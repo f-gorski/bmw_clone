5aec7938ddd4f3f46572aa9144251935b930ce3b
Fryderyk Górski <fryderykgorski@gmail.com>
2021-02-22 23:01:59 +0100

    feature: added navigation

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 
 import './styles/App.scss';
 import Home from './Layout/Home/Home';
+import Nav from './Layout/Navigation/Nav';
 
 const App = () => {
   return (
     <div className="App">
+      <Nav />
       <Home />
     </div>
   );
